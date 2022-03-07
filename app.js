3fafc1f04aa8b4022572f7af1ba5dfdbfa6769c1
unknown <rocketman_912@hotmail.com>
2022-03-07 16:08:28 -0600

    hasta antes de babel

--- app.js
+++ app.js
@@ -2,17 +2,29 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const path = require('path')
+
 const app = express()
+
 
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+
+// Rutas del Back
 app.get('/', (req, res) =>{
     res.send('hola Mundo')
 })
 
-app.set('puerto', process.env.PORT || 3000)
+// Middleware para Vue router mode history.
+const history = require('connect-history-api-fallback')
+app.use(history())
+app.use(express.static(path.join(__dirname, 'public')))
 
+
+app.set('puerto', process.env.PORT || 3000)
 app.listen(app.get('puerto'), () => {
     console.log('ejemplo del servidor puerto' + app.get('puerto'))
 })
